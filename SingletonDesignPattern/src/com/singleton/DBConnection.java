31ccf740f4a2fc0f31cc9e4bd41c46e2236ef40f
PavanReddy0604 <122519854+PavanReddy0604@users.noreply.github.com>
2023-08-19 22:50:06 +0530

    Singleton Design Pattern

---
+++ SingletonDesignPattern/src/com/singleton/DBConnection.java
@@ -0,0 +1,72 @@
+package com.singleton;
+
+//eager loading
+public class DBConnection {
+
+	public static DBConnection dbConnection = new DBConnection();
+
+	private DBConnection() {
+
+	}
+
+	public static DBConnection getInstance() {
+		return dbConnection;
+	}
+
+}
+
+//Lazy loading
+class DBConnection2 {
+
+	public static DBConnection2 dbConnection;
+
+	private DBConnection2() {
+
+	}
+
+	public static DBConnection2 getInstance() {
+		if (dbConnection == null) {
+			dbConnection = new DBConnection2();
+		}
+		return dbConnection;
+	}
+
+}
+
+//not suitable for multithreaded environments
+class DBConnection3 {
+	
+	public static DBConnection3 dbConnection;
+
+	private DBConnection3() {
+
+	}
+
+	public static synchronized DBConnection3 getInstance() {
+		if (dbConnection == null) {
+			dbConnection = new DBConnection3();
+		}
+		return dbConnection;
+	}
+}
+
+
+//Double Checked Locking
+class DBConnection4 {
+	
+	public static DBConnection4 dbConnection;
+	
+	private DBConnection4() {
+
+	}
+
+	public static synchronized DBConnection4 getInstance() {
+		if (dbConnection == null) {
+			synchronized (DBConnection4.class) {
+				dbConnection = new DBConnection4();
+			}
+
+		}
+		return dbConnection;
+	}
+}
